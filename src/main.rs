1e5da37c813955601675745e6214e770ce244ec2
Deltacat-terminal <undercat037@gmail.com>
2026-05-05 08:52:56 +0300

    license fix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// Copyright (C) 2026 Undercat037
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// aura-emerge: A gentoo-like wrapper for the aura AUR helper.
+
 use clap::Parser;
 use std::collections::HashSet;
 use std::fs;
